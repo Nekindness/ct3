1bffa57d83dd90a3f892c08a126752ea9260b926
Nekindness <137820039+Nekindness@users.noreply.github.com>
2024-04-27 18:59:48 +0300

    Productlist.js

--- src/Productlist.js
+++ src/Productlist.js
@@ -3,14 +3,12 @@ import ProductItem from './productitems';
 import style from './App.css';
 
 const ProductList = ({ products, onDelete }) => {
-  // Функция для обработки удаления продукта
   const handleDelete = (product) => {
     onDelete(product);
   };
 
   return (
     <div className="product-list">
-      {/* Отображение списка продуктов */}
       {products.map((product, index) => (
         <ProductItem key={index} product={product} onDelete={() => handleDelete(product)} />
       ))}
